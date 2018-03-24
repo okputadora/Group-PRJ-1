f4619356b2b9898aa0c87500561f4102e6d89836
moskowitza <moskowitzaaron@mac.com>
2018-03-24 13:46:22 -0400

    getting date ranges

--- js/application.js
+++ js/application.js
@@ -79,10 +79,12 @@ $(document).ready(function () {
 
         $(".date").each(function () {
             if ($(this).val() !== "") {
-                var date = $("#this").val();
+                var date = $(this).val();
+                console.log(date);
                 var ourFormat = "DDMMMMY";
                 var convertedStartDate = moment(date, ourFormat);
                 var startDate = moment(convertedStartDate).format("YYYYMMDD");
+                console.log(startDate);
                 var momentStart = moment(startDate);
                 var endDate=moment(momentStart).add(vacaLength, "day").format("YYYYMMDD");
                 console.log(endDate);
