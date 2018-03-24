091de825996c52c229ee065ddc1d1d89947f654f
okputadora <okputadora@gmail.com>
2018-03-24 10:10:18 -0400

    ok

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,18 @@
 
-// concerts sports food theater
-// var searchTerms = "SEARCH TERMS FROM THE FORM"
-// var location = "VACATION DESTINATIONS"
-// var dateRange = "DATE THE USER WANTS TO VACATION"
+var city1 = $("#city1").val()
+var city2 = $("#city2").val()
+var city3 = $("#city3").val()
+var city4 = $("#city4").val()
+var city5 = $("#city5").val()
+
+var searchTerms = "SEARCH TERMS FROM THE FORM"
+var location = "VACATION DESTINATIONS"
+var dateRange = "DATE THE USER WANTS TO VACATION"
 
 //api key xmnsRKbacpmsh6ZB83cvLNMQc4LTp1Znb3fjsngAa5M9Bt400S
 var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
 var api_key = 'qmFPcpp4ZnChQdF5';
-var api_url = 'http://api.eventful.com/json/events/search?app_key='+ api_key+'&keywords=books&location=San+Diego&date=Future';
+var api_url = 'http://api.eventful.com/json/events/search?app_key='+ api_key+'&keywords=concerts&location=San+Diego&date=Future';
 var request_url = cors_api_url + api_url;
 $.ajax({
   url: request_url,
@@ -34,11 +39,8 @@ $.ajax({
       startTime: event.start_time
     })
   })
-  .done(function(data) {
-    console.log(data);
-  });
 
-
+  console.log(parsedResults)
 
   //Call to Amadeus API
 
@@ -66,3 +68,4 @@ $.ajax({
       .then(function(response) {
     console.log(response);
       });
+})
