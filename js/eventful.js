400b1fe38bc3960a24576350761306ab19ba4204
okputadora <okputadora@gmail.com>
2018-03-26 01:17:45 -0400

    recursively hit api and build vacation object as we go

--- js/eventful.js
+++ js/eventful.js
@@ -5,119 +5,126 @@ var api_key = 'qmFPcpp4ZnChQdF5';
 
 $(document).on("ready", function(){
 
-//   var api_url = 'http://api.eventful.com/json/categories/list?app_key='+ api_key
-//   var request_url = cors_api_url + api_url;
-//   $.ajax({
-//     url: request_url,
-//     context: document.body
-//   })
-//   .done(function(data) {
-//     console.log("sUCCES")
-//     console.log(data);
-//   })
-//   // Very strange...the response is coming to us in the error handler!!!
-//   // but its not an error! Perhaps something to do with our CORS work around
-//   .error(function(error) {
-//     // we'll have to do our own error handling
-//     response = JSON.parse(error.responseText)
-//     console.log(response)
-//   })
-// })
   console.log("connected")
   $("#interestBtn").on("click", function(){
     // get the data from local storage
     var cities = JSON.parse(localStorage.cities)
-    var dateRanges = JSON.parse(localStorage.dateRanges)
     var homeCity = JSON.parse(localStorage.dateRanges)
+    var dateRanges = JSON.parse(localStorage.dateRanges)
     var interests = JSON.parse(localStorage.interests)
-    console.log("DATA FROM BACKEND")
-
-    console.log(cities)
-    console.log(dateRanges)
-    console.log(homeCity)
-    console.log(interests)
-    // this will be the callback function after all of the results have come in
-    function displayResults(vacations){
-      console.log(vacations)
-    }
     // build a request URL from the data
     var vacations = []
-    cities.forEach(function(city){
+    appendCity()
+    function appendCity(){
+      console.log("appending City")
+      // remove one city from the list
+      city = cities.shift()
+      // create a new vacation object and add the city to it
       var vacation = {
         city: city,
-        dateWindows:[]
+        dateWindows: []
       }
-      dateRanges.forEach(function(dateRange){
-        var dateWindow = {
-          startDate: dateRange.startDate,
-          endDate: dateRange.endDate,
-          interests: []
+      // it will be necessary to keep track of how many times we call appendDates
+      // because when we add interests to that specific date range we need to reference
+      // its position in the array of date windows
+      var dateIndex = 0
+      appendDates(vacation, dateIndex, function(){
+        // add the vacation to the list of possible vacations
+        vacations.push(vacation)
+        // if there are more cities repeat the process
+        if (cities.length !== 0){
+          // reset the dateRanges.
+          // to get the recursive functions to terminate we've been removing
+          // items from these lists...now we need to reset them because we're
+          // creating a new vacation object
+          dateRanges = JSON.parse(localStorage.dateRanges)
+          interests = JSON.parse(localStorage.interests)
+          appendCity()
         }
-        interests.forEach(function(interest){
-          var eventList = {
-            interestName: interest.interestName,
-            events: []
-          }
-          var api_url = 'http://api.eventful.com/json/events/search?app_key='+ api_key+
-          '&keywords='+interest.interestName +
-          '&category='+interest.category +
-          '&location='+ city.cityName +
-          '&date='+dateRange.startDate+"00-"+dateRange.endDate + '00';
-          var request_url = cors_api_url + api_url;
-          $.ajax({
-            url: request_url,
-            context: document.body
-          })
-          .done(function(data) {
-            console.log("sUCCES")
-            console.log(data.responseText);
-          })
-          // Very strange...the response is coming to us in the error handler!!!
-          // but its not an error! Perhaps something to do with our CORS work around
-          .error(function(error) {
-            // we'll have to do our own error handling
-            response = JSON.parse(error.responseText)
-            console.log(response)
-            var parsedResults = response.events.event.map(function(event){
-              return ({
-                title: event.title,
-                city: event.city_name,
-                state: event.region_name,
-                description: event.description,
-                venue: event.venue_name,
-                venueAddress: event.venue_address,
-                venueUrl: event.venue_url,
-                lat: event.latitude,
-                lon: event.longitude,
-                startTime: event.start_time
-              })
-            })
-          })
-          .then(function(){
-            console.log(parsedResults)
-            // put the events into the event list object
-            // buildObjecy(parsedResults)
-            eventList.events = parsedResults
-            // then put the eventList into the interest list of the datewindow object
-            dateWindow.interests.push(eventList)
-            // check if this is the last interest
-            // if (interestIndex === interests.length -1){
-            ////   callback function
-            // // then put the datewindow object into the vacation object
-                  //vacation.dateWindows.push(dateWindow)
-            //}
-            // and if this is the last interest of the last date window
-            // then push the date window to datewindow list
-            // and then if this is the last city put the whole thing to the
-          })
+        else {
+          console.log("===============END RESULT==============")
+          console.log(vacations)
+        }
+      })
+    }
+    function appendDates(vacation, dateIndex, callback){
+      console.log("appending dates")
+      // remove a datewindow from the list and add it to an object
+      // that will also store interests associated with that date (& city)
+      var window = dateRanges.shift()
+      var dateWindow = {
+        startDate: window.startDate,
+        endDate: window.endDate,
+        interests: []
+      }
+      // add it to the vacation object
+      vacation.dateWindows.push(dateWindow)
+      // pass the vacation  object off to appendInterests
+      appendInterests(vacation, dateIndex, function(){
+        // if theres more dateWindows repeat the process
+        if (dateRanges.length !== 0){
+          dateIndex += 1
+          // reset the interests
+          interests = JSON.parse(localStorage.interests)
+          appendDates(vacation, dateIndex, callback)
+        }
+        else{
+          callback()
+        }
+      })
+    }
+    function appendInterests(vacation, dateIndex, callback){
+      console.log("appending interests")
+      var interest = interests.shift()
+      interest = {
+        interestName: interest.interestName,
+        interestCategory: interest.interestCategory,
+        events: []
+      }
+      // get events from API
+      console.log("VACATION")
+      console.log(vacation)
+      var api_url = 'http://api.eventful.com/json/events/search?app_key='+ api_key+
+      '&keywords='+interest.interestName +
+      '&category='+interest.interestCategory +
+      '&location='+ vacation.city.cityName +
+      '&date='+ vacation.dateWindows[dateIndex].startDate+"00-"+vacation.dateWindows[dateIndex].endDate + '00';
+      console.log(api_url)
+      var request_url = cors_api_url + api_url;
+      $.ajax({
+        url: request_url,
+        context: document.body
+      })
+      .done(function(data){
+        console.log(data.responseText);
+      })
+      .error(function(error) {
+        // we'll have to do our own error handling
+        response = JSON.parse(error.responseText)
+        console.log(response)
+        var parsedResults = response.events.event.map(function(event){
+          return ({
+            title: event.title,
+            city: event.city_name,
+            state: event.region_name,
+            description: event.description,
+            venue: event.venue_name,
+            venueAddress: event.venue_address,
+            venueUrl: event.venue_url,
+            lat: event.latitude,
+            lon: event.longitude,
+            startTime: event.start_time
           })
         })
-
+        // add the results to our interes object
+        interest.events = parsedResults
+        // and add our interest object to our vacation object
+        vacation.dateWindows[dateIndex].interests.push(interest)
+        if (interests.length !== 0){
+          appendInterests(vacation, dateIndex, callback)
+        }
+        else {callback()}
       })
-      vacations.push(vacation)
-      setTimeout(function(){
-        console.log("VACATIONS")
-        console.log(vacations)
-      }, 10000)
-    })
+    }
   })
+})
